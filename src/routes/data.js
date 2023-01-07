ae24ab22baa5cd32833f104f05d054f78f9c4654
cou9 <y4yasertalha@gmail.com>
2023-01-07 12:26:33 +0530

    delete

--- src/routes/data.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const dataController = require("../cotrollers/dataController");
-
-router.post("/updateToken", dataController.updateToken);
-
-module.exports = router;
--- src/routes/user.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const userController = require("../cotrollers/userController");
-
-router.post("/signUp", userController.setUser);
-router.post("/updateToken", userController.updateToken);
-router.get("/allUsers", userController.allUsers);
-router.get("/:id", userController.getUser);
-
-module.exports = router;
